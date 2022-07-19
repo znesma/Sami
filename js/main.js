170b291cc80d0e4b8ca707672f009d4625a88b71
znesma <eng.nesma.dimofinf@gmail.com>
2022-07-19 15:25:23 +0200

    edit1

--- js/main.js
+++ js/main.js
@@ -12,6 +12,13 @@ $(function (){
         }
     })
 
+    // add active class by JQuery
+
+    $('.navbar ul li ').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+
     //works
     $(".works .box").hover(function(){
         $(this).addClass("box-text")
@@ -20,9 +27,39 @@ $(function (){
 
     })
 
-   // counter
-   $(".counter").counterUp({
-    delay: 20,
-    time: 1000
-});
+    // counter
+    $(".counter").counterUp({
+        delay: 40,
+        time: 2000
+    });
+
+    // Scroll to top
+    $(window).scroll(function () {
+
+        if($(window).scrollTop() >= 1000) {
+
+            $('.scroll').fadeIn(200);
+        } else {
+            $('.scroll').fadeOut(2000);
+        }
+
+    });
+
+    // click on the scroll button
+
+    $('.scroll').on("click", function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop:0
+            
+        }, 500);
+        
+        $('.navbar ul li').removeClass('active');
+        $('.navbar .home').addClass('active');
+        
+    });
+
+
 })
